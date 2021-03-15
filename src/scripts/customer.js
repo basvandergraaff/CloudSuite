244d12bf16f1848579c759b5bda86724d7daa0aa
bas <basvandergraaff@gmail.com>
2021-03-15 12:11:55 +0100

    topbar elements

--- src/scripts/customer.js
+++ src/scripts/customer.js
@@ -6,22 +6,36 @@
     window.onload = function() {
 
       var menuButton = document.getElementById("menu");
-      menuButton.onclick = menuOpen; 
-     }
-    
+      menuButton.onclick = toggleMenu; 
 
-    function menuOpen() {
-      console.log('aap');
+      var menuButton = document.getElementById("greyed");
+      menuButton.onclick = closeMenu; 
 
+      window.addEventListener('resize', function () { 
+        //"use strict";
+        //window.location.reload(); 
+        console.log('resize');
+        closeMenu();
+      });
+    }
+  
+    function toggleMenu() {
+      //console.log('click toggleMenu');
       if (!menuopen) {
-        document.getElementById("muted").style.visibility = 'visible';
-        document.getElementById("sub-menu").style.visibility = 'visible';
+        document.getElementById("greyed").style.visibility = 'visible';
+        document.getElementById("submenu").style.visibility = 'visible';
       }
       else {
-        document.getElementById("muted").style.visibility = 'hidden';
-        document.getElementById("sub-menu").style.visibility = 'hidden';
+        document.getElementById("greyed").style.visibility = 'hidden';
+        document.getElementById("submenu").style.visibility = 'hidden';
       }
       menuopen = !menuopen;
+    }
+    function closeMenu() {
+      //console.log('closeMenu');
+      document.getElementById("greyed").style.visibility = 'hidden';
+      document.getElementById("submenu").style.visibility = 'hidden';
+      menuopen = false;
     }
 
 })()
